d5d2b264a6d588d069dd89ad8b11efeb0a766f68
DESKTOP-JAGH2QT\DOMINIONXVII <dominic.b.gendron@gmail.com>
2018-10-30 17:48:28 -0400

    New cam

---
+++ Assets/Scripts/Util/LightFlicker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    [SerializeField] SinFloat intensity1;
+    [SerializeField] SinFloat intensity2;
+    [SerializeField] SinFloat range1;
+    [SerializeField] SinFloat range2;
+    Light light;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        light.intensity = (intensity1 + intensity2) / 2;
+        light.range = (range1 + range2) / 2;
+    }
+
+}
