03e110d66fb702a38a780a5f5ddb99855a9cc7d9
Guillaume Raymond <guiray@users.noreply.github.com>
2018-12-15 16:58:26 -0500

    Chagner lune par autre chose

---
+++ Assets/Scripts/Biostuff/FractalTreeControler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalTreeControler : FractalTree {
+
+    [SerializeField] LerpFloat lengthAnimation;
+    [SerializeField] LerpFloat angleAnimation;
+
+    public void Update()
+    {
+        lengthFactorOverDepth = lengthAnimation;
+        angleFactorOverDepth = angleAnimation;
+        UpdateStorm();
+    }
+}
